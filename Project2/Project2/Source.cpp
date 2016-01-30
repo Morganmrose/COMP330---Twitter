67a35b3ad5e831198828511b21d9fda5c14f14bb
Morgan Rose <morganmrose@aim.com>
2016-01-29 23:50:59 -0600

    Added Ideas for twitter tests

---
+++ Project2/Project2/Source.cpp
@@ -0,0 +1,10 @@
+/*Ideas for Twitter Testing
+
+-Test for number of
+	-URLS
+	-Characters less than, equal to, greater than certain numbers
+	-Hashtags
+	-Changing colors of text based on Hashtags, URLs, etc
+	-Correctness odf date/time of post
+	-Containment of special characters - !@?><, etc
+	-Contains numbers
