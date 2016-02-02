c8b51ae34f5829c731b207a942def91b5d7c09f1
Morgan Rose <morganmrose@aim.com>
2016-02-01 20:26:13 -0600

    Started basic structures to build off of

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -1,3 +1,8 @@
+#include <stdio.h>      /* printf */
+#include <assert.h> 
+#include <iostream>
+
+using namespace std;
 /*Ideas for Twitter Testing
 
 -Test for number of
@@ -8,3 +13,28 @@
 	-Correctness odf date/time of post
 	-Containment of special characters - !@?><, etc
 	-Contains numbers
+
+
+*/ 
+
+
+//need to create working / implemented classes
+class Message
+{
+public:
+	double length;   // Length of a message
+
+
+};
+
+
+int main(void)
+{
+	cout << "Enter a twitter message: \n";
+	char tmessage;
+	cin >> tmessage;
+	cout << tmessage;
+
+	system("pause");
+}
+
