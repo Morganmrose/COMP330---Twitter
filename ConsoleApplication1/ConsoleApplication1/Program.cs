30eb0bff8e7e9d89c18bb327be5090961c48cb68
Morgan Rose <morganmrose@aim.com>
2016-02-02 22:30:01 -0600

    Researched that unit testing is more user friendly in C# rather than C++, so researched more C# and redid code in C# from C++.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Twitter
+{
+    public static void Main()
+    {
+        Console.WriteLine("Tweet something!\n");
+        string str = Console.ReadLine();
+
+        Console.WriteLine("1) The length of '{0}' is ", str, str.Length);
+
+        int length = str.Length;
+        Console.ReadLine();
+    }
+}
