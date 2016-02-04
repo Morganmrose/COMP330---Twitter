a536ebd99063b064279c512ecbdc73b92884b3f5
Morgan Rose <morganmrose@aim.com>
2016-02-03 22:58:09 -0600

    Fixed the mentions error

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -7,33 +7,43 @@ class Twitter
         Console.WriteLine("Tweet something!\n");
         string str = Console.ReadLine();
         int length = str.Length;
-        Console.WriteLine("The length of '{0}' is {1}", str, str.Length);
-
-      
-    
-        str = str.Replace(" ", "");
+        string strx = str.Replace(" ", "");
         
         int Uppercase_count = 0;
         int Lowercase_count = 0;
         int mentions = 0;
         int digitsCount = str.Count(c => char.IsDigit(c));
 
-        foreach(char input in str)
+        foreach (char input in str)
+        {
+            if (input == '@') { mentions++; }
+            else if (Char.IsUpper(input))
             {
-                if (Char.IsUpper(input))
-                    Uppercase_count++;
-
-                else if (Char.IsLower(input))
-                    Lowercase_count++;
-                else if (str.Contains("@"))
-                    mentions++;
-
+                Uppercase_count++;
             }
-        Console.WriteLine("The number of characters without blank spaces are {0}", str.Length);
-        Console.WriteLine("The number of uppercase letters in this tweet is: {0}", Uppercase_count);
-        Console.WriteLine("The number of lowercase letters in this tweet is: {0}", Lowercase_count);
-        Console.WriteLine("The number of numbers in this tweet is: {0}", digitsCount);
-        Console.WriteLine("The number of mentions in this tweet are: {0}", mentions);
+            else if (Char.IsLower(input))
+            {
+                Lowercase_count++;
+            }
+ 
+
+        }
+
+        if (str.Length <= 140)
+        {
+
+            Console.WriteLine("The length of the full tweet: {0}", str.Length);
+            Console.WriteLine("The number of characters without blank spaces: {0}", strx.Length);
+            Console.WriteLine("The number of uppercase letters: {0}", Uppercase_count);
+            Console.WriteLine("The number of lowercase letters: {0}", Lowercase_count);
+            Console.WriteLine("Number count: {0}", digitsCount);
+            Console.WriteLine("The number of mentions: {0}", mentions);
+        }
+        else
+        {
+            Console.WriteLine("Your tweet exceeds the maxium number of characters (140). Try again.");
+            Main();
+        }
 
 
         
