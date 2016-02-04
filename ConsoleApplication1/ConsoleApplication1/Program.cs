f10d1f1822cac861a8c604cad081dc837c1b6ea4
Morgan Rose <morganmrose@aim.com>
2016-02-03 21:33:06 -0600

    Added more of the testing content

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -13,6 +13,20 @@ class Twitter
     
         str = str.Replace(" ", "");
         Console.WriteLine("The number of characters without blank spaces are {0}", str.Length); //
+        int Uppercase_count = 0;
+        int Lowercase_count = 0;
+        foreach(char input in str)
+            {
+                if (Char.IsUpper(input))
+                   Uppercase_count++;
+
+                else if (Char.IsLower(input))
+                    Lowercase_count++;
+
+
+            }
+        Console.WriteLine("The number of uppercase letters in this tweet is: {0}", Uppercase_count);
+        Console.WriteLine("The number of lowercase letters in this tweet is: {0}", Lowercase_count);
 
         
         Console.ReadLine();
