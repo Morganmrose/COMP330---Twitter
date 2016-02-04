35dae2b467a95eaba9b87cb8a8c4795fa6758700
Morgan Rose <morganmrose@aim.com>
2016-02-04 17:18:50 -0600

    Added more tests - True/False for number of mentions and topics. Gave a static tweet to run tests on.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System;
 using System.Linq;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 namespace Twitter
@@ -8,49 +8,38 @@ namespace Twitter
 
     public class Twitter
     {
-        private string m_tweet;
-
-        private double uppercase = 0;
         private double m_length = 0;
-
         private Twitter()
         {
         }
 
-        public Twitter(string tweet)
-        {
-            m_tweet = tweet;
-
-        }
-
-        public string Tweet
-        {
-            get { return m_tweet; }
-        }
-
-        public double Length
+        public double Length //gets length of tweet
         {
             get { return m_length; }
         }
 
-
         public static void Main()
         {
+            DateTime date = DateTime.Now;
             int Uppercase_count = 0;
             int Lowercase_count = 0;
             int mentions = 0;
+            bool ment;
+            int topic = 0;
+            //int top = 0;
             int special_char = 0;
 
 
-            Console.WriteLine("Tweet something!");
-            string str = Console.ReadLine();
-            Twitter ba = new Twitter(str);
+            //Console.WriteLine("Tweet something!"); Can create your own string
+            // string str = Console.ReadLine(); 
+            string str = "@ franky goes to #Hollywood.";
             string strx = str.Replace(" ", "");
 
             foreach (char input in str)
             {
                 if (input == '@') { mentions++; }
-                else if (input == '!' || input == '?' || input == '"' || input == '#' || input == '$' || input == '%' || input == '^' || input == '&' || input == '*' ||
+                else if (input == '#') { topic++; }
+                else if (input == '!' || input == '?' || input == '"' || input == '$' || input == '%' || input == '^' || input == '&' || input == '*' ||
                     input == '-' || input == '+' || input == '=' || input == '_' || input == '[' || input == ']' || input == '(' || input == ')' || input == ';' ||
                         input == ':' || input == '/' || input == ',' || input == '>' || input == '<')
                 //repetitive, but having problems implementing a regex
@@ -68,10 +57,11 @@ namespace Twitter
                 }
 
             }
-
-            if (str.Length <= 140)
+            if (str.Length <= 140) //outputs data
             {
                 int digitsCount = str.Count(c => char.IsDigit(c));
+                Console.WriteLine("Your tweet is:");
+                Console.WriteLine(str + "\n");
                 Console.WriteLine("The length of the full tweet: {0}", str.Length);
                 Console.WriteLine("The number of characters without blank spaces: {0}", strx.Length);
                 Console.WriteLine("The number of uppercase letters: {0}", Uppercase_count);
@@ -79,16 +69,22 @@ namespace Twitter
                 Console.WriteLine("Number count: {0}", digitsCount);
                 Console.WriteLine("The number of special characters: {0}", special_char);
                 Console.WriteLine("The number of mentions: {0}", mentions);
+                if (mentions == 1) { Console.WriteLine("Contains 1 mention: True"); }
+                else { Console.WriteLine("No mentions!"); }
+                if (topic == 1) { Console.WriteLine("Contains 1 topic: True"); }
+                else { Console.WriteLine("No topics!"); }
+                Console.WriteLine("Written on {0}", date);
+
+
+                Debug.Assert(date != null, "There's a problem with your computer's time system."); //Debug Assertion for unit test
+
+
+                {
+                    Debug.Assert(str.Length <= 140, "Your tweet exceeds the maximum number of characters"); //Debug Assertion for unit test
+                }
+                Console.ReadLine();
             }
-            else
-            {
-                Console.WriteLine("Your tweet exceeds the maxium number of characters (140). Try again.");
-                Main();
-            }
-
-
-
-            Console.ReadLine();
         }
     }
 }
+    
