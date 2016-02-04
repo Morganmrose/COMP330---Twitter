719605c12bab275f858baffe93d246f37a2b12a1
Morgan Rose <morganmrose@aim.com>
2016-02-03 22:02:05 -0600

    Now checks for mentions

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -15,23 +15,26 @@ class Twitter
         
         int Uppercase_count = 0;
         int Lowercase_count = 0;
-
-        
+        int mentions = 0;
         int digitsCount = str.Count(c => char.IsDigit(c));
 
         foreach(char input in str)
             {
                 if (Char.IsUpper(input))
-                   Uppercase_count++;
+                    Uppercase_count++;
 
                 else if (Char.IsLower(input))
                     Lowercase_count++;
+                else if (str.Contains("@"))
+                    mentions++;
 
             }
         Console.WriteLine("The number of characters without blank spaces are {0}", str.Length);
         Console.WriteLine("The number of uppercase letters in this tweet is: {0}", Uppercase_count);
         Console.WriteLine("The number of lowercase letters in this tweet is: {0}", Lowercase_count);
         Console.WriteLine("The number of numbers in this tweet is: {0}", digitsCount);
+        Console.WriteLine("The number of mentions in this tweet are: {0}", mentions);
+
 
         
         Console.ReadLine();
